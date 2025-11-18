3146bc44b9eeff197b8c964fc6911b0703d25374
gchukura <211885343+gchukura@users.noreply.github.com>
2025-11-18 04:22:38 -0500

    Block static assets from being crawled

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,45 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/admin', '/api/sync', '/api/revalidate'],
+        disallow: [
+          // Admin and internal pages
+          '/admin',
+          '/api',
+          
+          // Test and development pages
+          '/test-',
+          '/simple-test',
+          '/test-brewery',
+          '/test-programmatic',
+          '/test-simple-programmatic',
+          '/test-templates',
+          
+          // Next.js internal files and static assets (blocks CSS, JS, fonts, images)
+          '/_next/static',
+          '/_next/image',
+          '/_next/webpack-hmr',
+          
+          // Static asset directories (if any exist in public)
+          '/static',
+          
+          // Manifest and other non-content files
+          '/manifest.json',
+        ],
+      },
+      // Allow Googlebot to access static assets for proper rendering
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [
+          '/admin',
+          '/api',
+          '/test-',
+          '/simple-test',
+          '/test-brewery',
+          '/test-programmatic',
+          '/test-simple-programmatic',
+          '/test-templates',
+        ],
       },
     ],
     sitemap: [`${base}/sitemap.xml`],
